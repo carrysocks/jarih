c146cbfb94dd3a91ca96107c7db4a388e00e661a
carrysocks <choi73737@gmail.com>
2024-03-15 02:30:59 +0900

    fix: cors

--- gbus-api/app/core/middleware/cors.py
+++ gbus-api/app/core/middleware/cors.py
@@ -3,14 +3,19 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = ["http://localhost", "http://localhost:8080", "http://localhost:3000", 
-           "http://gbus-front.s3-website.ap-northeast-2.amazonaws.com", "http://jarih.net"
-           ]
+def set_cors(app: FastAPI) -> None:
+    origins = [
+        "http://localhost", 
+        "http://localhost:8080", 
+        "http://localhost:3000", 
+        "http://gbus-front.s3-website.ap-northeast-2.amazonaws.com", 
+        "http://jarih.net"
+    ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
--- gbus-api/app/main.py
+++ gbus-api/app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI, Depends
-from starlette.middleware.cors import CORSMiddleware
 
 from app.api.routes.api import api_router
 from app.core.config import get_app_settings
 from app.core.settings.app import AppSettings
 from app.db.dependencies import init_db
+from app.core.middleware.cors import set_cors
 
 
 def get_application() -> FastAPI:
@@ -12,15 +12,7 @@ def get_application() -> FastAPI:
     settings = get_app_settings()
     application.include_router(api_router)
 
-    origins = ["http://localhost", "http://localhost:80", "http://localhost:3000", "http://www.jarih.net", "http://jarih.net", "http://220.117.11.63:3000"]
-
-    application.add_middleware(
-        CORSMiddleware,
-        allow_origins=origins,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+    set_cors(application)
 
     init_db("postgresql://myuser:mypassword@localhost/mydb")
 
